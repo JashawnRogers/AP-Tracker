d64868cbf00d40bf6c4b7c664a636c321070d291
JashawnRogers <rogersjashawn20@gmail.com>
2026-06-21 09:55:05 -0700

    Initial commit

---
+++ src/main/java/com_jashawn/ap_tracker/ApTrackerApplication.java
@@ -0,0 +1,13 @@
+package com_jashawn.ap_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com_jashawn/ap_tracker/ApTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com_jashawn.ap_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
